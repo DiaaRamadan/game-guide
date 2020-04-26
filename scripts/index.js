1b42f001128d27f7e6fa4cec00df993511949b91
Diaa Ramadan <diaa.attie@elmnassa.com>
2020-04-26 21:53:53 +0200

    setup loader

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,22 @@
 const guideList = document.querySelector('.guides');
 const setupGuides = (data) => {
-    let html = "";
-    data.forEach(doc => {
-       const guide = doc.data();
-       const li = `
+    guideList.innerHTML = "<div class='center'><img class='image-responsive' src='img/ajax-loader.gif' alt='Loader'></div>";
+    if (data.length) {
+        let html = "";
+        data.forEach(doc => {
+            const guide = doc.data();
+            const li = `
             <li>
                 <div class="collapsible-header grey lighten-4">${guide.title}</div>
                 <div class="collapsible-body white">${guide.content}</div>
             </li>
        `;
-       html += li;
-    });
-    guideList.innerHTML = html;
+            html += li;
+        });
+        guideList.innerHTML = html;
+    }else{
+        guideList.innerHTML = "<h4 class='center'>Login to view guides</h4>";
+    }
 };
 
 // setup materialize components
